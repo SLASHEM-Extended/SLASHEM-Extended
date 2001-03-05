cd02c16e07a08d124be20720d63a320f42290ac8
j_ali <j_ali>
2001-03-05 23:53:33 +0000

    Change patchlev.h to match patchlevel.h

--- include/patchlev.h
+++ include/patchlev.h
@@ -32,8 +32,13 @@
 #define COPYRIGHT_BANNER_B \
 "NetHack, Copyright 1985-2000 Stichting Mathematisch Centrum, M. Stephenson."
 
+#ifdef MAC_MPW
+#define COPYRIGHT_BANNER_C \
+"         See license for details. Bug reports to hurtley@acm.org"
+#else
 #define COPYRIGHT_BANNER_C \
 "         See license for details. Bug reports to wac@intergate.bc.ca"
+#endif
 
 #if 0
 /*
