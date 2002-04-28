1b7f6fdb3f865a5f38173de80b5b771e1e966fdd
paulhurtley <paulhurtley>
2002-04-28 23:36:36 +0000

    Merge with NetHack 3.4.0

--- include/macconf.h
+++ include/macconf.h
@@ -1,4 +1,4 @@
-/*	SCCS Id: @(#)macconf.h	3.3	99/10/25	*/
+/*	SCCS Id: @(#)macconf.h	3.4	1999/10/25	*/
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -9,10 +9,10 @@
 /*
  * Compiler selection is based on the following symbols:
  *
- *	applec			MPW 68K compiler
- *  macintosh		MPW PPC compiler
+ *  __SC__			sc, a MPW 68k compiler
+ *  __MRC__			mrc, a MPW PowerPC compiler
  *	THINK_C			Think C compiler
- *	__MWERKS__		Metrowerks compiler
+ *	__MWERKS__		Metrowerks' Codewarrior compiler
  *
  * We use these early in config.h to define some needed symbols,
  * including MAC.
@@ -21,78 +21,51 @@
  # WIDENED_PROTOTYPES (defined if UNWIDENED_PROTOTYPES is undefined and
  # NHSTDC is defined).
  */
-#  ifdef applec
-#   define MAC_MPW32		/* Headers, and for avoiding a bug */
-#  endif
 
-#  ifndef __powerc
-#   define MAC68K		/* 68K mac (non-powerpc) */
-#  endif
+#ifndef __powerc
+# define MAC68K		/* 68K mac (non-powerpc) */
+#endif
 
-#  define RANDOM
-#  define NO_SIGNAL		/* You wouldn't believe our signals ... */
-#  define FILENAMELEN 256
-#  define NO_TERMS		/* For tty port (see wintty.h) */
+#define RANDOM
+#define NO_SIGNAL		/* You wouldn't believe our signals ... */
+#define FILENAMELEN 256
+#define NO_TERMS		/* For tty port (see wintty.h) */
 
-#  define TEXTCOLOR		/* For Mac TTY interface */
-#  define CHANGE_COLOR
+#define TEXTCOLOR		/* For Mac TTY interface */
+#define CHANGE_COLOR
 
-/* Use these includes instead of system.h. */
+/* Use these two includes instead of system.h. */
 #include <string.h>
 #include <stdlib.h>
-#include <time.h>
 
 /* Uncomment this line if your headers don't already define off_t */
 /*typedef long off_t;*/
+#include <time.h>	/* for time_t */
 
 /*
  * Try and keep the number of files here to an ABSOLUTE minimum !
  * include the relevant files in the relevant .c files instead !
  */
 #include <MacTypes.h>
-/*
- * Turn off the Macsbug calls for the production version.
- */
-#if 0
-#  undef Debugger
-#  undef DebugStr
-#  define Debugger()
-#  define DebugStr(aStr)
-#endif
-
-#ifdef MAC_MPW
-# define getpid() 1
-# define getuid() 1
-#endif /* MAC_MPW */
 
 /*
  * We could use the PSN under sys 7 here ...
+ * ...but it wouldn't matter...
  */
-#ifndef __CONDITIONALMACROS__	/* universal headers */
-#  define getpid() 1
-#  define getuid() 1
-#endif
-#  define index strchr
-#  define rindex strrchr
+#define getpid() 1
+#define getuid() 1
+#define index strchr
+#define rindex strrchr
 
-#  define Rand random
-
-#ifdef MAC_MPW
-# define error progerror
-#endif /* MAC_MPW */
+#define Rand random
 extern void error(const char *,...);
 
-# if !defined(O_WRONLY)
-#  ifdef __MWERKS__
-#include <unix.h>
-#   ifndef O_EXCL
-     /* MW 4.5 doesn't have this, so just use a bogus value */
-#    define O_EXCL 0x80000000
-#   endif
-#  else
-#include <fcntl.h>
-#  endif
+#if !defined(O_WRONLY)
+# ifdef __MWERKS__
+#  include <unix.h>
 # endif
+# include <fcntl.h>
+#endif
 
 /*
  * Don't redefine these Unix IO functions when making LevComp or DgnComp for
@@ -108,6 +81,8 @@ extern void error(const char *,...);
 # define lseek macseek
 #endif
 
+#define YY_NEVER_INTERACTIVE 1
+
 # define TEXT_TYPE 'TEXT'
 # define LEVL_TYPE 'LEVL'
 # define BONE_TYPE 'BONE'
@@ -115,6 +90,7 @@ extern void error(const char *,...);
 # define PREF_TYPE 'PREF'
 # define DATA_TYPE 'DATA'
 # define MAC_CREATOR 'slEm' /* Registered with DTS */
+# define TEXT_CREATOR 'ttxt' /* Something the user can actually edit */
 
 /*
  * Define PORT_HELP to be the name of the port-specfic help file.
