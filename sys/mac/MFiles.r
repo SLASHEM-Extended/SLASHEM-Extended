6c06359aff6fa9070d2421473f30a655d0b62d3c
paulhurtley <paulhurtley>
2002-09-15 21:02:48 +0000

    Use #define for version number

---
+++ sys/mac/MFiles.r
@@ -0,0 +1,14 @@
+#include "date.h"
+#include "patchlevel.h"
+
+resource 'vers' (1) {
+	VERSION_MAJOR, (VERSION_MINOR<<4) | PATCHLEVEL, beta, EDITLEVEL, verUS,
+	VERSION_STRING,
+	"Recover " VERSION_STRING
+};
+
+resource 'vers' (2) {
+	VERSION_MAJOR, (VERSION_MINOR<<4) | PATCHLEVEL, beta, EDITLEVEL, verUS,
+	VERSION_STRING,
+	"for Slash'EM " VERSION_STRING " (www.slashem.org)"
+};
