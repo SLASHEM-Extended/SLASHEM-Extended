5f0ab4ea62777ea869887688cd1e3beba9bf1519
paulhurtley <paulhurtley>
2003-08-02 23:55:44 +0000

    Add sLem version resource as required by Mac coding ruiles

--- sys/mac/Files.r
+++ sys/mac/Files.r
@@ -4,6 +4,12 @@
 #include "date.h"
 #include "patchlevel.h"
 
+type 'sLem' as 'STR ';
+resource 'sLem' (0, purgeable) {
+	"Slash'EM " VERSION_STRING
+	" Copyright © Stichting Mathematisch Centrum, Amsterdam, 1985Ð2003"
+};
+
 resource 'vers' (1, purgeable) {
 	VERSION_MAJOR, (VERSION_MINOR<<4) | PATCHLEVEL, beta, EDITLEVEL, verUS,
 	VERSION_STRING,
@@ -52,18 +58,18 @@ resource 'STR '(-16396, purgeable) {	/* Will be copied to the saved file. */
 	"Slash'EM"							/* See Inside Mac VI, page 9-21.	 */
 };
 
-read 'File' (1000,"cmdhelp") ":dat:cmdhelp";
-read 'File' (1001,"help") ":dat:help";
-read 'File' (1002,"hh") ":dat:hh";
-read 'File' (1003,"history") ":dat:history";
-read 'File' (1004,"license") ":dat:license";
-read 'File' (1005,"MacHelp") ":sys:mac:MacHelp";
-read 'File' (1006,"News") ":sys:mac:News";
-read 'File' (1007,"opthelp") ":dat:opthelp";
-read 'File' (1008,"wizhelp") ":dat:wizhelp";
-read 'File' (1019,"data") "::lib:data";
-read 'File' (1020,"dungeon") "::lib:dungeon";
-read 'File' (1041,"options") "::lib:options";
-read 'File' (1043,"oracles") "::lib:oracles";
-read 'File' (1045,"quest.dat") "::lib:quest.dat";
-read 'File' (1046,"rumors") "::lib:rumors";
+read 'File' (1000,"cmdhelp")	":dat:cmdhelp";
+read 'File' (1001,"help")		":dat:help";
+read 'File' (1002,"hh")			":dat:hh";
+read 'File' (1003,"history")	":dat:history";
+read 'File' (1004,"license")	":dat:license";
+read 'File' (1005,"MacHelp")	":sys:mac:MacHelp";
+read 'File' (1006,"News")		":sys:mac:News";
+read 'File' (1007,"opthelp")	":dat:opthelp";
+read 'File' (1008,"wizhelp")	":dat:wizhelp";
+read 'File' (1019,"data")		"::lib:data";
+read 'File' (1020,"dungeon")	"::lib:dungeon";
+read 'File' (1041,"options")	"::lib:options";
+read 'File' (1043,"oracles")	"::lib:oracles";
+read 'File' (1045,"quest.dat")	"::lib:quest.dat";
+read 'File' (1046,"rumors")		"::lib:rumors";
