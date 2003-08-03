468cc4c6c34c3c5e80c4caf9fe96fd0af3dc709f
paulhurtley <paulhurtley>
2003-08-03 00:05:45 +0000

    Add missing slRc version resource

--- sys/mac/MFiles.r
+++ sys/mac/MFiles.r
@@ -1,6 +1,11 @@
 #include "date.h"
 #include "patchlevel.h"
 
+type 'slRc' as 'STR ';
+resource 'slRc' (0, purgeable) {
+	"Recover " VERSION_STRING " Copyright © David Hairston 1993"
+};
+
 resource 'vers' (1) {
 	VERSION_MAJOR, (VERSION_MINOR<<4) | PATCHLEVEL, beta, EDITLEVEL, verUS,
 	VERSION_STRING,
