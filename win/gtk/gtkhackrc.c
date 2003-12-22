acba5e30a82b9327ce557cb4fa5c2c49d172ecdd
j_ali <j_ali>
2003-12-22 09:30:10 +0000

    Fix a problem with writing profile values out that aren't recognized and which contain an empty vector.
    It's possible to hit this error by using a CVS version of gtkhack to
    create a profile containing a connections profile with no flags set
    and then attempting to use the same profile with a proxified version
    of the GTK interface (which doesn't interpret "connections"). It's
    not possible to hit this in a released version of Slash'EM so I'm
    not bothering to log a proper bug report.

--- win/gtk/gtkhackrc.c
+++ win/gtk/gtkhackrc.c
@@ -1,4 +1,4 @@
-/* $Id: gtkhackrc.c,v 1.9 2003-12-13 22:58:28 j_ali Exp $ */
+/* $Id: gtkhackrc.c,v 1.10 2003-12-22 09:30:10 j_ali Exp $ */
 /* Copyright (c) Slash'EM Development Team 2003 */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -534,9 +534,11 @@ static GString *print_value(GtkHackRcValue *var)
 	    return str;
 	case PARSE_VALUE_TYPE_VECTOR:
 	    str = g_string_new("[");
-	    vl = print_value_list(&var->u.vector);
-	    g_string_append_len(str, vl->str, vl->len);
-	    g_string_free(vl, TRUE);
+	    if (var->u.vector.n_values) {
+		vl = print_value_list(&var->u.vector);
+		g_string_append_len(str, vl->str, vl->len);
+		g_string_free(vl, TRUE);
+	    }
 	    g_string_append_c(str, ']');
 	    return str;
 	case PARSE_VALUE_TYPE_VALUE_LIST:
