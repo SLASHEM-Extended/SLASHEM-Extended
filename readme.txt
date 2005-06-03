242a3b4ade6966ba02da138e6872bf996a52f238
j_ali <j_ali>
2005-06-03 01:04:27 +0000

    Fixed bug 1213881: Lieutenants lack Firearms

--- readme.txt
+++ readme.txt
@@ -74,6 +74,7 @@ ver 0.0.7E7F1 [?] [Released by ?]
 		    conduct (Nico Kist)
 -fixed bug 924811: Fire vampires leave corpses
 -fixed bug 1028948: casting stone to flesh on a gargoyle
+-fixed bug 1213881: Lieutenants lack Firearms
 
 See history.txt for changes made in previous versions.
 
--- src/makemon.c
+++ src/makemon.c
@@ -278,7 +278,7 @@ register struct monst *mtmp;
 			  	m_initthrow(mtmp, BULLET, 30);
 			  	m_initthrow(mtmp, BULLET, 30);
 			  }
-			  w1 = rn2(2) ? KNIFE : DAGGER;
+			  w2 = rn2(2) ? KNIFE : DAGGER;
 			  if (rn2(2)) {
 			  	(void) mongets(mtmp, FRAG_GRENADE);
 			  	(void) mongets(mtmp, FRAG_GRENADE);
