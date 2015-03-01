ecd11528ce2700a77db1947b9761207721c92983
Edoardo Spadolini <kerio00@gmail.com>
2015-03-01 10:40:18 +0100

    Fix version.sh when CC is not cc

--- util/version.sh
+++ util/version.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cc -E - <<EOF | tail -n1 | sed "s/ /./g"
+${1:-cc} -E - <<EOF | tail -n1 | sed "s/ //g"
 #include "include/patchlevel.h"
-VERSION_MAJOR VERSION_MINOR PATCHLEVEL
+VERSION_MAJOR . VERSION_MINOR . PATCHLEVEL
 EOF
