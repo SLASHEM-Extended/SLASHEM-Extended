bddbc29ba7e5bec6b05f4acb6771abb742bde1e3
AmyBSOD <failureclock@gmx.de>
2015-05-11 08:02:08 +0200

    Statues of very small monsters may contain spellbooks

--- src/mkobj.c
+++ src/mkobj.c
@@ -869,7 +869,7 @@ boolean artif;
 		    case STATUE:
 			/* possibly overridden by mkcorpstat() */
 			otmp->corpsenm = rndmonnum();
-			if (!verysmall(&mons[otmp->corpsenm]) &&
+			if ( (!verysmall(&mons[otmp->corpsenm]) || !rn2(10) ) &&
 				rn2(level_difficulty()/2 + 10) > 10)
 			    (void) add_to_container(otmp,
 						    mkobj(SPBOOK_CLASS,FALSE));
