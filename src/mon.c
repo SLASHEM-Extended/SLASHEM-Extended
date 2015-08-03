b9c9eb42ba8257feea41eef2e73e099d645e561f
AmyBSOD <failureclock@gmx.de>
2015-08-03 08:20:30 +0200

    The "useful item" dummy monster doesn't delete stuff from its inventory.

--- src/mon.c
+++ src/mon.c
@@ -2666,7 +2666,7 @@ register struct monst *mdef;
 
 		/* reduce amount of musable items the player can use --Amy */
 		/* item stealers usually won't delete stuff, since their stuff might actually be your original stuff! */
-			if (is_musable(obj) && !rn2(3) && !dmgtype(mdef->data, AD_SEDU) && !dmgtype(mdef->data, AD_SITM) && (!dmgtype(mdef->data, AD_SSEX) || !rn2(3) ) && (!dmgtype(mdef->data, AD_SGLD) || !rn2(5) ) ) delobj(obj);
+			if (is_musable(obj) && !rn2(3) && !(mdef->data == &mons[PM_GOOD_ITEM_MASTER]) && !dmgtype(mdef->data, AD_SEDU) && !dmgtype(mdef->data, AD_SITM) && (!dmgtype(mdef->data, AD_SSEX) || !rn2(3) ) && (!dmgtype(mdef->data, AD_SGLD) || !rn2(5) ) ) delobj(obj);
 		    else (void) add_to_container(otmp, obj);
 		}
 #ifndef GOLDOBJ
