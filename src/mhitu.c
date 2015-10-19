SLASHEM-Extended/SLASHEM-Extended

commit 39f88880a0dd680df9626fc212191d5baa726744

Chokhmah damage is increased by the sin counter

src/mhitu.c
@@ -4060,6 +4060,7 @@ dopois:
 		hitmsg(mtmp, mattk);
 		if (mtmp->mcan) break;
 		dmg += u.chokhmahdamage;
+		dmg += rnd(u.ualign.sins);
 		u.chokhmahdamage++;
 		break;
 
@@ -6172,6 +6173,7 @@ do_stone:
 			You("are pummeled with heavy debris!");
 			if (mtmp->mcan) break;
 			tmp += u.chokhmahdamage;
+			tmp += rnd(u.ualign.sins);
 			u.chokhmahdamage++;
 		  break;
 
@@ -8370,6 +8372,7 @@ gazemu(mtmp, mattk)	/* monster gazes at you */
                   pline("%s gazes at you and screams the word 'DIE!'", Monnam(mtmp));
 		    stop_occupation();
 			dmgplus += u.chokhmahdamage;
+			dmgplus += rnd(u.ualign.sins);
 			u.chokhmahdamage++;
                   mdamageu(mtmp, d(3,8) + dmgplus);
 		  }

src/trap.c
@@ -2278,7 +2278,7 @@ glovecheck:		(void) rust_dmg(uarmg, "gauntlets", 1, TRUE, &youmonst);
 		seetrap(trap);
 		pline("You stepped on a trigger!");
 		pline("Something hits your %s.", body_part(rn2(19) ) );
-		losehp(1 + u.chokhmahdamage,"escalating trap",KILLED_BY_AN);
+		losehp(1 + u.chokhmahdamage + rnd(u.ualign.sins),"escalating trap",KILLED_BY_AN);
 		u.chokhmahdamage++;
 		break;
 

src/uhitm.c
@@ -4239,6 +4239,7 @@ uchar aatyp;
 		if (mhit) {
 			pline("WHACK! You feel like you just got whipped!");
 			tmp += u.chokhmahdamage;
+			tmp += rnd(u.ualign.sins);
 			u.chokhmahdamage++;
 			mdamageu(mon, tmp);
 		}

commit 25519387291ca13e51d1b744c0768434b30e80d4

Fix error message flood for chokhmah attacks
39f8888 introduced this... well, at least it's fixed now.

src/mhitu.c
@@ -4060,7 +4060,7 @@ dopois:
 		hitmsg(mtmp, mattk);
 		if (mtmp->mcan) break;
 		dmg += u.chokhmahdamage;
-		dmg += rnd(u.ualign.sins);
+		dmg += rnd(u.ualign.sins + 1);
 		u.chokhmahdamage++;
 		break;
 
@@ -6173,7 +6173,7 @@ do_stone:
 			You("are pummeled with heavy debris!");
 			if (mtmp->mcan) break;
 			tmp += u.chokhmahdamage;
-			tmp += rnd(u.ualign.sins);
+			tmp += rnd(u.ualign.sins + 1);
 			u.chokhmahdamage++;
 		  break;
 
@@ -8372,7 +8372,7 @@ gazemu(mtmp, mattk)	/* monster gazes at you */
                   pline("%s gazes at you and screams the word 'DIE!'", Monnam(mtmp));
 		    stop_occupation();
 			dmgplus += u.chokhmahdamage;
-			dmgplus += rnd(u.ualign.sins);
+			dmgplus += rnd(u.ualign.sins + 1);
 			u.chokhmahdamage++;
                   mdamageu(mtmp, d(3,8) + dmgplus);
 		  }

src/trap.c
@@ -2280,7 +2280,7 @@ glovecheck:		(void) rust_dmg(uarmg, "gauntlets", 1, TRUE, &youmonst);
 		seetrap(trap);
 		pline("You stepped on a trigger!");
 		pline("Something hits your %s.", body_part(rn2(19) ) );
-		losehp(1 + u.chokhmahdamage + rnd(u.ualign.sins),"escalating trap",KILLED_BY_AN);
+		losehp(1 + u.chokhmahdamage + rnd(u.ualign.sins + 1),"escalating trap",KILLED_BY_AN);
 		u.chokhmahdamage++;
 		break;
 

src/uhitm.c
@@ -4247,7 +4247,7 @@ uchar aatyp;
 		if (mhit) {
 			pline("WHACK! You feel like you just got whipped!");
 			tmp += u.chokhmahdamage;
-			tmp += rnd(u.ualign.sins);
+			tmp += rnd(u.ualign.sins + 1);
 			u.chokhmahdamage++;
 			mdamageu(mon, tmp);
 		}