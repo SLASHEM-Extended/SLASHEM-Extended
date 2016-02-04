27dc200b8d8234dbfa82325f9da965d471a8db3e
AmyBSOD <failureclock@gmx.de>
2016-02-04 08:12:46 +0100

    Don't automatically know enchantment of gems, balls and chains

--- src/objects.c
+++ src/objects.c
@@ -3254,11 +3254,11 @@ WAND((char *)0,        "knobbed",  0, 500, 1, 0,         IRON,     HI_METAL),
 /* gems ... - includes stones and rocks but not boulders */
 #define GEM(name,desc,prob,wt,gval,sdam,ldam,nutr,mohs,glass,color) OBJECT( \
 	    OBJ(name,desc), \
-	    BITS(0,1,0,0,0,0,0,0,0,HARDGEM(mohs),0,-P_SLING,glass), 0, \
+	    BITS(0,1,1,0,0,0,0,0,0,HARDGEM(mohs),0,-P_SLING,glass), 0, \
 	    GEM_CLASS, prob, 0, 1, gval, sdam, ldam, 0, 0, nutr, color )
 #define ROCK(name,desc,kn,prob,wt,gval,sdam,ldam,mgc,nutr,mohs,glass,color) OBJECT( \
 	    OBJ(name,desc), \
-	    BITS(kn,1,0,0,mgc,0,0,0,0,HARDGEM(mohs),0,-P_SLING,glass), 0, \
+	    BITS(kn,1,1,0,mgc,0,0,0,0,HARDGEM(mohs),0,-P_SLING,glass), 0, \
 	    GEM_CLASS, prob, 0, wt, gval, sdam, ldam, 0, 0, nutr, color )
 
 /* when adding new gems, adjust gemprobs in o_init.c --Amy */
@@ -3439,79 +3439,79 @@ OBJECT(OBJ("boulder",(char *)0), BITS(1,0,0,0,0,0,0,0,1,0,0,P_NONE,MINERAL), 0,
 OBJECT(OBJ("statue", (char *)0), BITS(1,0,0,1,0,0,0,0,0,0,0,P_NONE,MINERAL), 0,
 		ROCK_CLASS,  4500, 0, 2500,  0, 20, 20, 0, 0, 200, CLR_WHITE),
 
-OBJECT(OBJ("heavy iron ball", (char *)0), BITS(1,0,0,0,0,0,0,0,0,0,WHACK,P_FLAIL,IRON), 0,
+OBJECT(OBJ("heavy iron ball", (char *)0), BITS(1,0,1,0,0,0,0,0,0,0,WHACK,P_FLAIL,IRON), 0,
 		BALL_CLASS,  3735, 0,  1200, 10, 25, 25, 0, 0,  200, HI_METAL),
 						/* +d4 when "very heavy" */
-OBJECT(OBJ("heavy stone ball", (char *)0), BITS(1,0,0,0,0,0,0,0,0,0,WHACK,P_FLAIL,MINERAL), 0,
+OBJECT(OBJ("heavy stone ball", (char *)0), BITS(1,0,1,0,0,0,0,0,0,0,WHACK,P_FLAIL,MINERAL), 0,
 		BALL_CLASS,  200, 0,  1200, 10, 25, 25, 0, 0,  200, CLR_WHITE),
-OBJECT(OBJ("heavy glass ball", (char *)0), BITS(1,0,0,0,0,0,0,0,0,0,WHACK,P_FLAIL,GLASS), 0,
+OBJECT(OBJ("heavy glass ball", (char *)0), BITS(1,0,1,0,0,0,0,0,0,0,WHACK,P_FLAIL,GLASS), 0,
 		BALL_CLASS,  100, 0,  1200, 10, 25, 25, 0, 0,  200, CLR_BRIGHT_BLUE),
-OBJECT(OBJ("quite heavy iron ball", (char *)0), BITS(1,0,0,0,0,0,0,0,0,0,WHACK,P_FLAIL,IRON), 0,
+OBJECT(OBJ("quite heavy iron ball", (char *)0), BITS(1,0,1,0,0,0,0,0,0,0,WHACK,P_FLAIL,IRON), 0,
 		BALL_CLASS,  500, 0,  1600, 10, 35, 35, 0, 0,  200, CLR_GREEN),
-OBJECT(OBJ("heavy gold ball", (char *)0), BITS(1,0,0,0,0,0,0,0,0,0,WHACK,P_FLAIL,GOLD), 0,
+OBJECT(OBJ("heavy gold ball", (char *)0), BITS(1,0,1,0,0,0,0,0,0,0,WHACK,P_FLAIL,GOLD), 0,
 		BALL_CLASS,  100, 0,  1600, 10, 35, 35, 0, 0,  200, CLR_YELLOW),
-OBJECT(OBJ("heavy clay ball", (char *)0), BITS(1,0,0,0,0,0,0,0,0,0,WHACK,P_FLAIL,MINERAL), 0,
+OBJECT(OBJ("heavy clay ball", (char *)0), BITS(1,0,1,0,0,0,0,0,0,0,WHACK,P_FLAIL,MINERAL), 0,
 		BALL_CLASS,  100, 0,  1600, 10, 35, 35, 0, 0,  200, CLR_BROWN),
-OBJECT(OBJ("really heavy iron ball", (char *)0), BITS(1,0,0,0,0,0,0,0,0,0,WHACK,P_FLAIL,IRON), 0,
+OBJECT(OBJ("really heavy iron ball", (char *)0), BITS(1,0,1,0,0,0,0,0,0,0,WHACK,P_FLAIL,IRON), 0,
 		BALL_CLASS,  50, 0,  2000, 10, 45, 45, 0, 0,  200, CLR_RED),
-OBJECT(OBJ("heavy elysium ball", (char *)0), BITS(1,0,0,0,0,0,0,0,0,0,WHACK,P_FLAIL,DRAGON_HIDE), 0,
+OBJECT(OBJ("heavy elysium ball", (char *)0), BITS(1,0,1,0,0,0,0,0,0,0,WHACK,P_FLAIL,DRAGON_HIDE), 0,
 		BALL_CLASS,  50, 0,  2000, 10, 45, 45, 0, 0,  200, CLR_BRIGHT_CYAN),
-OBJECT(OBJ("heavy granite ball", (char *)0), BITS(1,0,0,0,0,0,0,0,0,0,WHACK,P_FLAIL,MINERAL), 0,
+OBJECT(OBJ("heavy granite ball", (char *)0), BITS(1,0,1,0,0,0,0,0,0,0,WHACK,P_FLAIL,MINERAL), 0,
 		BALL_CLASS,  50, 0,  2000, 10, 45, 45, 0, 0,  200, CLR_BRIGHT_MAGENTA),
-OBJECT(OBJ("extremely heavy iron ball", (char *)0), BITS(1,0,0,0,0,0,0,0,0,0,WHACK,P_FLAIL,IRON), 0,
+OBJECT(OBJ("extremely heavy iron ball", (char *)0), BITS(1,0,1,0,0,0,0,0,0,0,WHACK,P_FLAIL,IRON), 0,
 		BALL_CLASS,  5, 0,  2400, 10, 55, 55, 0, 0,  200, CLR_ORANGE),
-OBJECT(OBJ("heavy conundrum ball", (char *)0), BITS(1,0,0,0,0,0,0,0,0,0,WHACK,P_FLAIL,DRAGON_HIDE), 0,
+OBJECT(OBJ("heavy conundrum ball", (char *)0), BITS(1,0,1,0,0,0,0,0,0,0,WHACK,P_FLAIL,DRAGON_HIDE), 0,
 		BALL_CLASS,  5, 0,  2400, 10, 55, 55, 0, 0,  200, CLR_RED),
-OBJECT(OBJ("heavy concrete ball", (char *)0), BITS(1,0,0,0,0,0,0,0,0,0,WHACK,P_FLAIL,MINERAL), 0,
+OBJECT(OBJ("heavy concrete ball", (char *)0), BITS(1,0,1,0,0,0,0,0,0,0,WHACK,P_FLAIL,MINERAL), 0,
 		BALL_CLASS,  5, 0,  2400, 10, 55, 55, 0, 0,  200, CLR_BRIGHT_GREEN),
-OBJECT(OBJ("impossibly heavy iron ball", (char *)0), BITS(1,0,0,0,0,0,0,0,0,0,WHACK,P_FLAIL,IRON), 0,
+OBJECT(OBJ("impossibly heavy iron ball", (char *)0), BITS(1,0,1,0,0,0,0,0,0,0,WHACK,P_FLAIL,IRON), 0,
 		BALL_CLASS,  40, 0,  12000, 10, 75, 75, 0, 0,  200, CLR_BLACK),
-OBJECT(OBJ("impossibly heavy glass ball", (char *)0), BITS(1,0,0,0,0,0,0,0,0,0,WHACK,P_FLAIL,GLASS), 0,
+OBJECT(OBJ("impossibly heavy glass ball", (char *)0), BITS(1,0,1,0,0,0,0,0,0,0,WHACK,P_FLAIL,GLASS), 0,
 		BALL_CLASS,  20, 0,  12000, 10, 75, 75, 0, 0,  200, CLR_BRIGHT_BLUE),
-OBJECT(OBJ("impossibly heavy mineral ball", (char *)0), BITS(1,0,0,0,0,0,0,0,0,0,WHACK,P_FLAIL,MINERAL), 0,
+OBJECT(OBJ("impossibly heavy mineral ball", (char *)0), BITS(1,0,1,0,0,0,0,0,0,0,WHACK,P_FLAIL,MINERAL), 0,
 		BALL_CLASS,  20, 0,  12000, 10, 75, 75, 0, 0,  200, CLR_WHITE),
-OBJECT(OBJ("impossibly heavy elysium ball", (char *)0), BITS(1,0,0,0,0,0,0,0,0,0,WHACK,P_FLAIL,DRAGON_HIDE), 0,
+OBJECT(OBJ("impossibly heavy elysium ball", (char *)0), BITS(1,0,1,0,0,0,0,0,0,0,WHACK,P_FLAIL,DRAGON_HIDE), 0,
 		BALL_CLASS,  20, 0,  12000, 10, 75, 75, 0, 0,  200, CLR_MAGENTA),
 
-OBJECT(OBJ("iron chain", (char *)0), BITS(1,0,0,0,0,0,0,0,0,0,WHACK,P_FLAIL,IRON), 0,
+OBJECT(OBJ("iron chain", (char *)0), BITS(1,0,1,0,0,0,0,0,0,0,WHACK,P_FLAIL,IRON), 0,
 		CHAIN_CLASS, 3735, 0,  120,  0,  4,  4, 0, 0,  200, HI_METAL),
 						/* +1 both l & s */
 
-OBJECT(OBJ("stone chain", (char *)0), BITS(1,0,0,0,0,0,0,0,0,0,WHACK,P_FLAIL,MINERAL), 0,
+OBJECT(OBJ("stone chain", (char *)0), BITS(1,0,1,0,0,0,0,0,0,0,WHACK,P_FLAIL,MINERAL), 0,
 		CHAIN_CLASS, 200, 0,  120,  0,  4,  4, 0, 0,  200, CLR_WHITE),
-OBJECT(OBJ("glass chain", (char *)0), BITS(1,0,0,0,0,0,0,0,0,0,WHACK,P_FLAIL,GLASS), 0,
+OBJECT(OBJ("glass chain", (char *)0), BITS(1,0,1,0,0,0,0,0,0,0,WHACK,P_FLAIL,GLASS), 0,
 		CHAIN_CLASS, 100, 0,  120,  0,  4,  4, 0, 0,  200, CLR_BRIGHT_BLUE),
 
-OBJECT(OBJ("rotating chain", (char *)0), BITS(1,0,0,0,0,0,0,0,0,0,WHACK,P_FLAIL,IRON), 0,
+OBJECT(OBJ("rotating chain", (char *)0), BITS(1,0,1,0,0,0,0,0,0,0,WHACK,P_FLAIL,IRON), 0,
 		CHAIN_CLASS, 500, 0,  240,  0,  8,  8, 0, 0,  200, CLR_BRIGHT_BLUE),
-OBJECT(OBJ("gold chain", (char *)0), BITS(1,0,0,0,0,0,0,0,0,0,WHACK,P_FLAIL,GOLD), 0,
+OBJECT(OBJ("gold chain", (char *)0), BITS(1,0,1,0,0,0,0,0,0,0,WHACK,P_FLAIL,GOLD), 0,
 		CHAIN_CLASS, 100, 0,  240,  0,  8,  8, 0, 0,  200, CLR_YELLOW),
-OBJECT(OBJ("clay chain", (char *)0), BITS(1,0,0,0,0,0,0,0,0,0,WHACK,P_FLAIL,MINERAL), 0,
+OBJECT(OBJ("clay chain", (char *)0), BITS(1,0,1,0,0,0,0,0,0,0,WHACK,P_FLAIL,MINERAL), 0,
 		CHAIN_CLASS, 100, 0,  240,  0,  8,  8, 0, 0,  200, CLR_BROWN),
 
-OBJECT(OBJ("scourge", (char *)0), BITS(1,0,0,0,0,0,0,0,0,0,WHACK,P_FLAIL,IRON), 0,
+OBJECT(OBJ("scourge", (char *)0), BITS(1,0,1,0,0,0,0,0,0,0,WHACK,P_FLAIL,IRON), 0,
 		CHAIN_CLASS, 50, 0,  360,  0,  15,  15, 0, 0,  200, CLR_BRIGHT_MAGENTA),
-OBJECT(OBJ("elysium scourge", (char *)0), BITS(1,0,0,0,0,0,0,0,0,0,WHACK,P_FLAIL,DRAGON_HIDE), 0,
+OBJECT(OBJ("elysium scourge", (char *)0), BITS(1,0,1,0,0,0,0,0,0,0,WHACK,P_FLAIL,DRAGON_HIDE), 0,
 		CHAIN_CLASS, 50, 0,  360,  0,  15,  15, 0, 0,  200, CLR_BRIGHT_CYAN),
-OBJECT(OBJ("granite scourge", (char *)0), BITS(1,0,0,0,0,0,0,0,0,0,WHACK,P_FLAIL,MINERAL), 0,
+OBJECT(OBJ("granite scourge", (char *)0), BITS(1,0,1,0,0,0,0,0,0,0,WHACK,P_FLAIL,MINERAL), 0,
 		CHAIN_CLASS, 50, 0,  360,  0,  15,  15, 0, 0,  200, CLR_GREEN),
 
-OBJECT(OBJ("nunchiaku", (char *)0), BITS(1,0,0,0,0,0,0,0,0,0,WHACK,P_FLAIL,IRON), 0,
+OBJECT(OBJ("nunchiaku", (char *)0), BITS(1,0,1,0,0,0,0,0,0,0,WHACK,P_FLAIL,IRON), 0,
 /* spelled wrong on purpose, as the samurai has another item with that name --Amy */
 		CHAIN_CLASS, 5, 0,  480,  0,  24,  24, 0, 0,  200, CLR_BRIGHT_GREEN),
 
-OBJECT(OBJ("conundrum nunchiaku", (char *)0), BITS(1,0,0,0,0,0,0,0,0,0,WHACK,P_FLAIL,DRAGON_HIDE), 0,
+OBJECT(OBJ("conundrum nunchiaku", (char *)0), BITS(1,0,1,0,0,0,0,0,0,0,WHACK,P_FLAIL,DRAGON_HIDE), 0,
 		CHAIN_CLASS, 5, 0,  480,  0,  24,  24, 0, 0,  200, CLR_RED),
-OBJECT(OBJ("concrete nunchiaku", (char *)0), BITS(1,0,0,0,0,0,0,0,0,0,WHACK,P_FLAIL,MINERAL), 0,
+OBJECT(OBJ("concrete nunchiaku", (char *)0), BITS(1,0,1,0,0,0,0,0,0,0,WHACK,P_FLAIL,MINERAL), 0,
 		CHAIN_CLASS, 5, 0,  480,  0,  24,  24, 0, 0,  200, CLR_ORANGE),
 
-OBJECT(OBJ("hostage chain", (char *)0), BITS(1,0,0,0,0,0,0,0,0,0,WHACK,P_FLAIL,IRON), 0,
+OBJECT(OBJ("hostage chain", (char *)0), BITS(1,0,1,0,0,0,0,0,0,0,WHACK,P_FLAIL,IRON), 0,
 		CHAIN_CLASS, 40, 0,  1200,  0,  32,  32, 0, 0,  200, CLR_BLACK),
-OBJECT(OBJ("glass hostage chain", (char *)0), BITS(1,0,0,0,0,0,0,0,0,0,WHACK,P_FLAIL,GLASS), 0,
+OBJECT(OBJ("glass hostage chain", (char *)0), BITS(1,0,1,0,0,0,0,0,0,0,WHACK,P_FLAIL,GLASS), 0,
 		CHAIN_CLASS, 20, 0,  1200,  0,  32,  32, 0, 0,  200, CLR_BRIGHT_BLUE),
-OBJECT(OBJ("mineral hostage chain", (char *)0), BITS(1,0,0,0,0,0,0,0,0,0,WHACK,P_FLAIL,MINERAL), 0,
+OBJECT(OBJ("mineral hostage chain", (char *)0), BITS(1,0,1,0,0,0,0,0,0,0,WHACK,P_FLAIL,MINERAL), 0,
 		CHAIN_CLASS, 20, 0,  1200,  0,  32,  32, 0, 0,  200, CLR_WHITE),
-OBJECT(OBJ("elysium hostage chain", (char *)0), BITS(1,0,0,0,0,0,0,0,0,0,WHACK,P_FLAIL,DRAGON_HIDE), 0,
+OBJECT(OBJ("elysium hostage chain", (char *)0), BITS(1,0,1,0,0,0,0,0,0,0,WHACK,P_FLAIL,DRAGON_HIDE), 0,
 		CHAIN_CLASS, 20, 0,  1200,  0,  32,  32, 0, 0,  200, CLR_MAGENTA),
 
 /* extra balls and chains for convict mainly (he can advance flail skill by swinging them) --Amy */
