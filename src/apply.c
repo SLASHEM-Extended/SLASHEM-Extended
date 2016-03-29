97ae03cbe7a7082fb8dc636da78535354ecb887f
AmyBSOD <failureclock@gmx.de>
2016-03-29 10:53:09 +0200

    Fix C343-424
    Lighting the menora while it has exactly one turn of light left should
    no longer render the game unwinnable.

--- src/apply.c
+++ src/apply.c
@@ -1017,7 +1017,7 @@ register struct obj *obj;
 	}
 	if (!invocation_pos(u.ux, u.uy)) {
 		pline_The("%s %s being rapidly consumed!", s, vtense(s, "are"));
-		obj->age /= 2;
+		obj->age = (obj->age + 1L) / 2L;
 	} else {
 		if(obj->spe == 7) {
 		    if (Blind)
