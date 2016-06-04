4d75a80cc3e9057d6d76d8025b48537e3eaf9c5c
AmyBSOD <failureclock@gmx.de>
2016-06-04 23:36:37 +0200

    Hotfix for "respawn bug" actual bug
    Thought it was fixed but it happened to Lorskel again. I botched the fix back then. Hopefully this time I nailed it!

--- src/mon.c
+++ src/mon.c
@@ -2737,7 +2737,7 @@ register struct monst *mtmp;
 	    }
 	}
 
-	if (RespawnProblem || u.uprops[RESPAWN_BUG].extrinsic || have_respawnstone() && tmp != PM_UNFORTUNATE_VICTIM && tmp != PM_SCROLLER_MASTER && tmp != PM_BOULDER_MASTER && tmp != PM_ITEM_MASTER && tmp != PM_GOOD_ITEM_MASTER && tmp != PM_BAD_ITEM_MASTER && tmp != PM_HOLE_MASTER && tmp != PM_TRAP_MASTER && !(mtmp->data->geno & G_UNIQ) ) {
+	if ( (RespawnProblem || u.uprops[RESPAWN_BUG].extrinsic || have_respawnstone() ) && tmp != PM_UNFORTUNATE_VICTIM && tmp != PM_SCROLLER_MASTER && tmp != PM_BOULDER_MASTER && tmp != PM_ITEM_MASTER && tmp != PM_GOOD_ITEM_MASTER && tmp != PM_BAD_ITEM_MASTER && tmp != PM_HOLE_MASTER && tmp != PM_TRAP_MASTER && !(mtmp->data->geno & G_UNIQ) ) {
 	    switch(rnd(10)) {
 		case 1:
 		case 2:
