a2b13c48ea66645ced0dc7c3a60ae5ca85c44356
AmyBSOD <failureclock@gmx.de>
2017-04-12 19:13:32 +0200

    Really fix cloak wearing

--- src/do_wear.c
+++ src/do_wear.c
@@ -4148,7 +4148,7 @@ boolean noisy;
 		if (yn("Try to put it on anyway?") == 'y') {
 			if (rn2(2)) { 	u.ublesscnt += rnz(5);
 			pline("Feeling uncomfortable, you decide to stop trying.");
-			nomul(-1, "putting on armor that didn't fit");			
+			nomul(-2, "putting on armor that didn't fit");			
 		    return 0;}
 			}
 
