e402968f0555dda9b77269874397a18763af42fa
AmyBSOD <failureclock@gmx.de>
2017-04-12 19:19:06 +0200

    Try again to fix the cloak-wearing bug
    Using an ugly hack because nomul seems to get overwritten somewhere along the armor-wearing function.

--- src/do_wear.c
+++ src/do_wear.c
@@ -4148,7 +4148,8 @@ boolean noisy;
 		if (yn("Try to put it on anyway?") == 'y') {
 			if (rn2(2)) { 	u.ublesscnt += rnz(5);
 			pline("Feeling uncomfortable, you decide to stop trying.");
-			nomul(-2, "putting on armor that didn't fit");			
+			nomul(-2, "putting on armor that didn't fit");
+			youmonst.movement = 0; /* ugly hack */
 		    return 0;}
 			}
 
