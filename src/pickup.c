3aa27f0f5b30439f7d87a5b53730bda0e4faf508
AmyBSOD <failureclock@gmx.de>
2017-05-26 08:09:17 +0200

    Unfix SC343-20 because SEGFAULT!!!
    Yay. It's bloody obvious, but I thought "hey, that code is from The Dev
    Team, so it must work!" Little did I know that NH360 is fixing it in a
    completely different way.

--- src/pickup.c
+++ src/pickup.c
@@ -1334,13 +1334,8 @@ boolean telekinesis;	/* not picking it up directly by hand */
 		obj->quan -= gold_capacity;
 		costly_gold(obj->ox, obj->oy, gold_capacity);
 	    } else {
+		/* The SC343-20 bugfix causes phantom crash bugs BECAUSE OBJ IS BEING DELETED. --Amy */
 		u.ugold += count;
-		/* fix SC343-20 by using the nh360 code --Amy */
-		if (count == obj->quan)
-		    delobj(obj);
-		else
-		    obj->quan -= count;
-		/* although hangup is generally cheating, and there are probably gazillions of other ways to exploit it :( */
 		if ((nearload = near_capacity()) != 0)
 		    pline("%s %ld gold piece%s.",
 			  nearload < MOD_ENCUMBER ?
@@ -1349,6 +1344,10 @@ boolean telekinesis;	/* not picking it up directly by hand */
 		else
 		    prinv((char *) 0, obj, count);
 		costly_gold(obj->ox, obj->oy, count);
+		if (count == obj->quan)
+		    delobj(obj);
+		else
+		    obj->quan -= count;
 	    }
 	    flags.botl = 1;
 	    if (flags.run) nomul(0, 0);
