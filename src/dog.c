ddfcadfee65a054732d92a1421e60ed16a74d90f
AmyBSOD <failureclock@gmx.de>
2017-10-09 10:05:19 +0200

    Hopefully fix taming monsters with food
    Previously, any food could tame a M2_DOMESTIC, even if it was a food
    type they can't eat, due to a mistake in an earlier commit.

--- src/dog.c
+++ src/dog.c
@@ -1196,6 +1196,10 @@ boolean guaranteed;
 
 		if (!is_domestic(mtmp->data) && !is_petty(mtmp->data) && !(guaranteed) )
 			return (struct monst *)0;
+
+		if (!(guaranteed) && obj && dogfood(mtmp, obj) >= MANFOOD)
+			return (struct monst *)0;
+
 		}
 
 	/* failsafe for things that REALLY cannot be tamed --Amy */
