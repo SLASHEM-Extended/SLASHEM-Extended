23a5ac3bf476fab47d36c424e4137c3fedaad66a
AmyBSOD <failureclock@gmx.de>
2018-12-21 23:51:28 +0100

    Backport cloned weapon bugfix

--- src/makemon.c
+++ src/makemon.c
@@ -14647,6 +14647,7 @@ xchar x, y;	/* clone's preferred location or 0 (near mon) */
 	m2->my = mm.y;
 
 	m2->minvent = (struct obj *) 0; /* objects don't clone */
+	MON_NOWEP(m2);
 	m2->mleashed = FALSE;
 #ifndef GOLDOBJ
 	m2->mgold = 0L;
