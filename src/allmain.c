90afc6384aa76e95123099fd9b5ac3eb9a319024
AmyBSOD <failureclock@gmx.de>
2020-01-10 16:17:12 +0100

    Start with 2 skill points

--- src/allmain.c
+++ src/allmain.c
@@ -13238,6 +13238,7 @@ boolean new_game;	/* false => restoring an old game */
 	if (new_game && Race_if(PM_MAGYAR)) u.weapon_slots += 1000;
 	if (new_game && Race_if(PM_YUGGER)) u.weapon_slots += 15;
 	if (new_game && Race_if(PM_RUSMOT)) u.weapon_slots += 5;
+	if (new_game) u.weapon_slots += 2;
 
 	if (new_game) u.zapem_mode = 0;
 	if ((flags.zapem || Role_if(PM_SOFTWARE_ENGINEER) || Role_if(PM_CRACKER) || Role_if(PM_JANITOR) || Role_if(PM_SPACE_MARINE) || Role_if(PM_STORMBOY) || Role_if(PM_YAUTJA) || Role_if(PM_QUARTERBACK) || Role_if(PM_PSYKER) || Role_if(PM_EMPATH) || Role_if(PM_MASTERMIND) || Role_if(PM_WEIRDBOY) || Role_if(PM_ASTRONAUT) || Race_if(PM_RETICULAN) || Race_if(PM_OUTSIDER) || Role_if(PM_CYBERNINJA) || Role_if(PM_DISSIDENT) || Role_if(PM_XELNAGA)) && new_game && !flags.wonderland && !flags.lostsoul && !flags.uberlostsoul) {
