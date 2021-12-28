418137071041a5c5ece7a796224141ce2c3430fd
AmyBSOD <failureclock@gmx.de>
2021-12-28 18:12:44 +0100

    Whooooooooops
    Fake amulets of Yendor shouldn't be so easy to get rid of. Thanks amateurhour for finding this oversight.

--- include/objclass.h
+++ include/objclass.h
@@ -109,7 +109,7 @@ struct objclass {
 /* all of them in mkobj.c now --Amy */
 
 #define is_unwitherable(otmp)	(objects[otmp->otyp].oc_material == MT_BRICK)
-#define hard_to_destruct(otmp)	(objects[otmp->otyp].oc_material == MT_ETERNIUM || otmp->stckcurse || (uimplant && uimplant->oartifact == ART_HENRIETTA_S_TENACIOUSNESS) || (otmp->oartifact == ART_SOFTSTEP) || (otmp->oartifact && otmp->cursed && (spec_ability(otmp, SPFX_EVIL)) ) )
+#define hard_to_destruct(otmp)	((otmp->otyp == FAKE_AMULET_OF_YENDOR) || objects[otmp->otyp].oc_material == MT_ETERNIUM || otmp->stckcurse || (uimplant && uimplant->oartifact == ART_HENRIETTA_S_TENACIOUSNESS) || (otmp->oartifact == ART_SOFTSTEP) || (otmp->oartifact && otmp->cursed && (spec_ability(otmp, SPFX_EVIL)) ) )
 #define is_etheritem(otmp)	(objects[otmp->otyp].oc_material == MT_ETHER)
 #define is_meteosteelitem(otmp)	(objects[otmp->otyp].oc_material == MT_METEOSTEEL)
 
