390b2fe3b65b0cb92141ba18087ce3065a688a53
AmyBSOD <failureclock@gmx.de>
2023-04-22 12:19:42 +0200

    Fix a SLASH'EM bug with kicking
    This bug has existed in SLASH'EM ever since they added the "need +X weapon to hit" attribute: if your boots have an enchantment value of -1 or worse, they cannot hurt any monster because all monsters count as "needs +0 or higher enchantment to hit" for that purpose due to this oversight. It doesn't do the same for weapons, a -1 weapon can still hit things, so... it's fixed now.

--- src/dokick.c
+++ src/dokick.c
@@ -896,7 +896,7 @@ register xchar x, y;
 		objenchant = uarmf->spe;
 	else objenchant = 0;
 
-	if (objenchant < canhitmon && !Upolyd && (issoviet || !rn2(isfriday ? 2 : 3) ) ) {
+	if ((objenchant < canhitmon) && (canhitmon > 0) && !Upolyd && (issoviet || !rn2(isfriday ? 2 : 3) ) ) {
 		if (!issoviet) Your("attack doesn't seem to harm %s.",
 			mon_nam(mon));
 		else pline("Etot monstr ne mozhet byt' povrezhden, potomu chto Sovetskiy khochet nesmotrya vas.");
